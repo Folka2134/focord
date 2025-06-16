5d7a3136122123404c053a0d376d9f6a7db0f0c1
folka2134 <folka2134@gmail.com>
2025-06-16 11:30:43 +0100

    chore: Creating express backend

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,11 @@
+export const signup = (req: any, res: any) => {
+  res.send("Sign Up Page");
+};
+
+export const login = (req: any, res: any) => {
+  res.send("Login Page");
+};
+
+export const logout = (req: any, res: any) => {
+  res.send("Logout Page");
+};
---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes";
+import dotenv from "dotenv";
+import { connectToDatabase } from "./lib/db";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectToDatabase();
+});
