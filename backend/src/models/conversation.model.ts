52422162c0f8c8b364a98a86df3416222072ce64
folka2134 <folka2134@gmail.com>
2025-06-17 16:49:55 +0100

    feat: Created conversation model

---
+++ backend/src/models/conversation.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+
+const conversationSchema = new Schema(
+  {
+    participants: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+    lastMessage: { type: Schema.Types.ObjectId, ref: "Message" },
+    isGroup: { type: Boolean, default: false },
+    name: { type: String },
+  },
+  { timestamps: true },
+);
+
+const Conversation = model("Conversation", conversationSchema);
+
+export default Conversation;
