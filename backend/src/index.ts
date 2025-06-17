e0ded665bce52567a068c65600b6a9ca0af3490a
folka2134 <folka2134@gmail.com>
2025-06-17 11:08:47 +0100

    feat: setup mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route";
+import { connectDb } from "./lib/connectDB";
 
 dotenv.config();
 const app = express();
@@ -11,4 +12,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
+  connectDb();
 });
