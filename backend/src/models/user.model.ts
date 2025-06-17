fb4bccdf014dee258328816dbeede72687cd982d
folka2134 <folka2134@gmail.com>
2025-06-17 11:49:22 +0100

    chore: Updated input requirements

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
-  email: { type: String, required: true },
-  userName: { type: String, required: true },
-  password: { type: String, required: true },
+  userName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
   avatarUrl: { type: String, default: "" },
 });
 
